de877de4c5d4735fc1755a7ef13a57a08a8091a7
letnagao <leticia-nagao@hotmail.com>
2022-06-11 19:36:46 -0300

    finishing footer info

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -64,7 +64,7 @@ function Footer() {
           <div class='social-media-wrap'>
             <div class='footer-logo'>
               <Link to='/' className='social-logo'>
-                TRVL
+                TRVL 
                 <i class='fab fa-typo3' />
               </Link>
             </div>
